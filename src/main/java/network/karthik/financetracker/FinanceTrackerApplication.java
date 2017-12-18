5fcec844bb268c1b2f1b4660ccade9ef3cc31075
KarthikRaja1388 <karthik.vbdon88@gmail.com>
2017-12-18 18:22:38 +1300

    added files

---
+++ src/main/java/network/karthik/financetracker/FinanceTrackerApplication.java
@@ -0,0 +1,12 @@
+package network.karthik.financetracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinanceTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinanceTrackerApplication.class, args);
+	}
+}
---
+++ src/main/java/network/karthik/financetracker/controller/AppController.java
@@ -0,0 +1,8 @@
+package network.karthik.financetracker.controller;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class AppController {
+
+}
